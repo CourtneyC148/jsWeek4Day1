0b81ef38a45b15085b63585fb9993bd2f7fec8ec
courtneyc148 <courtneyc148@gmail.com>
2020-10-19 21:29:11 -0400

    js week 4 day 1 homework

---
+++ app.js
@@ -0,0 +1,28 @@
+let objOne = {
+    name: "David",
+    age: 29,
+    friends: ["Adam", "Sean"]
+};
+
+let objTwo = {
+    name: "Andrew",
+    age: 21,
+    friends: ["Chris", "Kurt"]
+};
+
+let objThree = {
+    name: "Sarah",
+    age: 30,
+    friends: ["Christina", "Megan"]
+};
+
+console.log(objOne.name);
+console.log(objTwo.age);
+console.log(objThree.friends[1]);
+
+
+let arr = [1, 2, 3, "Hi", "Bye"];
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
